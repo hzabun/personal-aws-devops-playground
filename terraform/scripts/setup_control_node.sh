633cea25ac8cc03e9055b805da08898967fe79fc
Halil Zabun <halil.zabun@icloud.com>
2025-05-23 14:36:06 +0200

    chore: convert local variables to lowercase

--- terraform/scripts/setup_control_node.sh
+++ terraform/scripts/setup_control_node.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Keypair file name provided as input
-KEYPAIR_NAME=$1
-if [[ -z "$KEYPAIR_NAME" ]]; then
+keypair_name=$1
+if [[ -z "$keypair_name" ]]; then
   echo "Please provide a keypair file name"
   exit 1
 fi
 
 # Extract and convert IP addresses JSON
-EC2_IPS=()
+ec2_ips=()
 while IFS= read -r line; do
-  EC2_IPS+=("$line")
+  ec2_ips+=("$line")
 done < <(terraform output -json instance_ips | jq -r '.[]')
 
-if [[ -z "$EC2_IPS" ]]; then
+if [[ -z "$ec2_ips" ]]; then
   echo "Failed to retrieve EC2 IP address from TF state"
   exit 1
 fi
 
 # Split IP addresses
-CONTROL_NODE=${EC2_IPS[0]}
-MANAGED_NODES=("${EC2_IPS[@]:1}")
+control_node="${ec2_ips[0]}"
+managed_node=("${ec2_ips[@]:1}")
 
-TMP_FILE=$(mktemp)
-printf "%s\n" "${MANAGED_NODES[@]}" > "$TMP_FILE"
+tmp_file=$(mktemp)
+printf "%s\n" "${managed_node[@]}" > "$tmp_file"
 
 # # # Copy keypair and managed nodes to control node
-scp -i "${KEYPAIR_NAME}" "${KEYPAIR_NAME}" "ec2-user@${CONTROL_NODE}:~.ssh"
-scp -i "${KEYPAIR_NAME}" "${TMP_FILE}" "ec2-user@${CONTROL_NODE}:~.ssh/known_hosts"
+scp -i "$keypair_name" "$keypair_name" "ec2-user@${control_node}:~.ssh"
+scp -i "$keypair_name" "$tmp_file" "ec2-user@${control_node}:~.ssh/known_hosts"
 
 # # # SSH into control node
-ssh -i "${KEYPAIR_NAME}" "ec2-user@${CONTROL_NODE}"
+ssh -i "$keypair_name" "ec2-user@${control_node}"
