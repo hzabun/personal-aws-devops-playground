a994077ad4a77371bb130eb1c766265c53c1355e
Halil Zabun <halil.zabun@icloud.com>
2025-05-26 11:55:48 +0200

    feat: allow HTTPS egress

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -47,6 +47,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_http_https" {
   security_group_id = aws_security_group.allow_internet_traffic.id
 }
 
+
+resource "aws_vpc_security_group_egress_rule" "allow_https" {
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+  ip_protocol = "tcp"
+  security_group_id = aws_security_group.allow_internet_traffic.id
+}
+
 resource "aws_security_group" "allow_internal_ssh" {
   vpc_id = aws_vpc.main.id
   tags = merge(local.tags, {
