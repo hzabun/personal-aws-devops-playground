1af70cfdbb3e5edfac07e202f17b787ab388404b
Halil Zabun <halil.zabun@icloud.com>
2025-06-01 08:24:58 +0200

    refactor: replace relative paths with absolute project root paths

--- docker/build-and-push-image.sh
+++ docker/build-and-push-image.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
 # load credentials and account ID
-source ../secrets.env
-source ../ecr-repo.env
+source config/secrets.env
+source config/ecr-repo.env
 
 # build image
-docker build .. -t playground/flask-app
+docker build -f "docker/Dockerfile" -t playground/flask-app .
 
 # tag image for ECR
-docker tag $NAMESPACE/$REPO:latest $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/playground/flask-app:latest
+docker tag "$NAMESPACE/$REPO:latest" $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/playground/flask-app:latest
+
+# Check if ECR repo exists, create if not
+
+if ! aws ecr describe-repositories --repository-names "$NAMESPACE/$REPO" --region us-east-1 > /dev/null 2>&1; then
+  aws ecr create-repository --repository-name "$NAMESPACE/$REPO" --region us-east-1
+fi
 
 # push image to ECR
 docker push $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$NAMESPACE/$REPO:latest
--- docker/ecr-login.sh
+++ docker/ecr-login.sh
@@ -1,4 +1,6 @@
-source ../secrets.env
+#!/bin/bash
+
+source secrets.env
 
 # login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
