21b4c07f6ee58e0bb21bf8775b4606c92ece3d8a
Halil Zabun <halil.zabun@icloud.com>
2025-06-02 11:43:34 +0200

    chore: convert relative paths to absolute ones

--- scripts/setup_control_node.sh
+++ scripts/setup_control_node.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Keypair file name provided as input
-keypair_path="$(dirname "$0")/../ssh-keys/$1"
+keypair_path="./ssh-keys/$1"
 if [[ -z "$1" ]]; then
   echo "Please provide a keypair file name"
   exit 1
 fi
 
 # Extract public IP address of control node
-control_node_public_ip=$(terraform output -state="$(dirname "$0")/../terraform/terraform.tfstate" -json instance_public_ips | jq -r '.[0]' | xargs)
+control_node_public_ip=$(terraform output -state="./terraform/terraform.tfstate" -json instance_public_ips | jq -r '.[0]' | xargs)
 
 # Extract private IP addresses
 all_private_ips=()
 while IFS= read -r line; do
   all_private_ips+=("$line")
-done < <(terraform output -state="$(dirname "$0")/../terraform/terraform.tfstate" -json instance_private_ips | jq -r '.[]')
+done < <(terraform output -state="./terraform/terraform.tfstate" -json instance_private_ips | jq -r '.[]')
 
 if [[ ${#all_private_ips[@]} -eq 0 ]]; then
   echo "Failed to retrieve EC2 IP address from TF state"
