1fba6d48b0b4b636af7be9b0039ff532dec33202
Halil Zabun <halil.zabun@icloud.com>
2025-06-02 16:45:27 +0200

    chore: update paths and variable names

--- docker/build-and-push-image.sh
+++ docker/build-and-push-image.sh
@@ -5,13 +5,12 @@ source config/secrets.env
 source config/ecr-repo.env
 
 # build image
-docker build -f "docker/Dockerfile" -t playground/flask-app .
+docker build -f "docker/Dockerfile" -t "$NAMESPACE/$REPO" .
 
 # tag image for ECR
 docker tag "$NAMESPACE/$REPO:latest" $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/playground/flask-app:latest
 
 # Check if ECR repo exists, create if not
-
 if ! aws ecr describe-repositories --repository-names "$NAMESPACE/$REPO" --region us-east-1 > /dev/null 2>&1; then
   aws ecr create-repository --repository-name "$NAMESPACE/$REPO" --region us-east-1
 fi
