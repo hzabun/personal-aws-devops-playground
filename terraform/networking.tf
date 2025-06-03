cac27e0e6dfb58404e8c9851f9b41bf4aac7617b
Halil Zabun <halil.zabun@icloud.com>
2025-06-03 16:07:16 +0200

    chore: format file

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -31,7 +31,7 @@ resource "aws_route_table_association" "public_assoc" {
 
 resource "aws_security_group" "allow_internet_traffic" {
   vpc_id = aws_vpc.main.id
-  tags   = merge(local.tags, {
+  tags = merge(local.tags, {
     Name = "Basic internet traffic"
   })
 
@@ -49,10 +49,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_http_https" {
 
 
 resource "aws_vpc_security_group_egress_rule" "allow_https" {
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = 443
-  to_port = 443
-  ip_protocol = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
   security_group_id = aws_security_group.allow_internet_traffic.id
 }
 
