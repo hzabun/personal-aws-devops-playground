12a9710b3a79f9dd668de9c0ff46ec3104e0bf32
Halil Zabun <halil.zabun@icloud.com>
2025-06-04 15:06:26 +0200

    docs: add comments to python and dockerfile

--- app/hello.py
+++ app/hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Show basic HTML message at root level
 @app.route("/")
 def hello_world():
     return "<p>Hello, World from my containerized Flask app!</p>"
