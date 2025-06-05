4c5263b29543be1aef75417505e76f02427bf4df
Halil Zabun <halil.zabun@icloud.com>
2025-06-05 07:52:38 +0200

    fix: use buildx to build linux/amd64 images

--- docker/build-and-push-image.sh
+++ docker/build-and-push-image.sh
@@ -4,16 +4,16 @@
 source config/secrets.env
 source config/ecr-repo.env
 
-# build image
-docker build -f "docker/Dockerfile" -t "$NAMESPACE/$REPO" .
-
-# tag image for ECR
-docker tag "$NAMESPACE/$REPO:latest" $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/playground/flask-app:latest
-
 # Check if ECR repo exists, create if not
 if ! aws ecr describe-repositories --repository-names "$NAMESPACE/$REPO" --region us-east-1 > /dev/null 2>&1; then
   aws ecr create-repository --repository-name "$NAMESPACE/$REPO" --region us-east-1
 fi
 
-# push image to ECR
-docker push $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$NAMESPACE/$REPO:latest
+# Set up new builder
+docker buildx create --use || true
+
+# Build, tag and push image to ECR
+docker buildx build --platform linux/amd64 \
+  -f "docker/Dockerfile" \
+  -t $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$NAMESPACE/$REPO:latest \
+  --push .
