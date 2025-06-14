5c0e6aca4bd8288ddd4d5f539e5fe1e328d76251
Halil <halil.zabun@icloud.com>
2025-06-14 16:09:05 +0200

    build: reuse existing Docker builder to enable image build cache

--- ecs/docker/build-and-push-image.sh
+++ ecs/docker/build-and-push-image.sh
@@ -4,13 +4,20 @@
 source config/secrets.env
 source config/ecr-repo.env
 
+# login to ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+
 # Check if ECR repo exists, create if not
 if ! aws ecr describe-repositories --repository-names "$NAMESPACE/$REPO" --region us-east-1 > /dev/null 2>&1; then
   aws ecr create-repository --repository-name "$NAMESPACE/$REPO" --region us-east-1
 fi
 
-# Set up new builder
-docker buildx create --use || true
+# Set up new builder only if it doesn't exist
+if ! docker buildx inspect mybuilder &>/dev/null; then
+  docker buildx create --name mybuilder --use
+else
+  docker buildx use mybuilder
+fi
 
 # Build, tag and push image to ECR
 docker buildx build --platform linux/amd64 \
--- ecs/docker/ecr-login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ./config/secrets.env
-
-# login to ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
