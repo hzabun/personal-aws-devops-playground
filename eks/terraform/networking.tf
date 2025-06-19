aa85a6e0ad2c8eecc1d7804f942154517f1e2290
Halil Zabun <halil.zabun@icloud.com>
2025-06-19 08:17:11 +0200

    feat: add NAT gateways

--- eks/terraform/networking.tf
+++ eks/terraform/networking.tf
@@ -82,3 +82,61 @@ resource "aws_internet_gateway" "main_igw" {
     Name = "main"
   }
 }
+
+resource "aws_eip" "nat1" {
+  domain = "vpc"
+}
+
+resource "aws_eip" "nat2" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public_subnet1.id
+
+  depends_on = [aws_internet_gateway.main_igw]
+}
+
+resource "aws_nat_gateway" "nat2" {
+  allocation_id = aws_eip.nat2.id
+  subnet_id     = aws_subnet.public_subnet2.id
+
+  depends_on = [aws_internet_gateway.main_igw]
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat1.id
+  }
+
+  tags = merge(local.tags, {
+    Name = "private-rt-1"
+  })
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat2.id
+  }
+
+  tags = merge(local.tags, {
+    Name = "private-rt-2"
+  })
+}
+
+resource "aws_route_table_association" "private_assoc1" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private1.id
+}
+
+resource "aws_route_table_association" "private_assoc2" {
+  subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private2.id
+}
