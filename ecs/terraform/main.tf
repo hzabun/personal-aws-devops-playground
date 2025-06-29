f7f7f0cd474814e5d5da069a0af04335c44257be
Halil <halil.zabun@icloud.com>
2025-06-29 18:13:33 +0200

    feat(ecs): add S3 remote backend

--- ecs/terraform/main.tf
+++ ecs/terraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket  = "demo-terraform-state-bucket"
+    key     = "terraform/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
